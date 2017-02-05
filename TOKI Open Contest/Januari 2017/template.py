7bf9ee51f447a7f5e706cb6437e5386a3000c1ae
deega- <ivan.ega.p@gmail.com>
2017-02-05 10:25:47 +0700

    Install VSCode

---
+++ TOKI Open Contest/Januari 2017/template.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def read(mode=2):
+    inp = input().strip()
+    if mode == 0: return inp
+    if mode == 1: return inp.split()
+    if mode == 2: return list(map(int, inp.split()))
